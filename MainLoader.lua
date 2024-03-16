4d68b481d133bb8a0d5c14c0a621ed1faf15d0ff
D0NTL1GHT <notlight.artem@gmail.com>
2024-03-16 23:41:35 +0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -2,4 +2,8 @@
 repeat wait() until game:IsLoaded()
 
 local pid = game.PlaceId;
-local baseLink = ""
+local baseLink = "raw.githubusercontent.com/D0NTL1GHTDEV/AntiDeath.cc/main/";
+
+if (pid == 4639625707) then
+    loadstring(game:HttpGet(baseLink .. "WarTycoon/wartycoon.lua", true))()
+end
